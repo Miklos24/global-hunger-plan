f734805f6212a9fe0e1593f954f25014d8b6963b
Miklos Bowling <miklos.bowling@gmail.com>
2022-03-12 17:01:33 -0800

    Added link to democracyio and death counter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import Button from '@mui/material/Button';
+import Create from '@mui/icons-material/Create';
+import Typography from '@mui/material/Typography';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [seconds, setSeconds] = useState(0);
+
+  useEffect(() => {
+    let interval = setInterval(() => {
+      setSeconds(seconds => seconds + 1);
+    }, 3456)
+    return ()=> clearInterval(interval);
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
+      <div className="title-section">
+        <Typography variant="h2">
+          <span> Since you've arrived on this site, {seconds} people have died of hunger-related causes. </span>
+        </Typography>
+      </div>
+      <div className="democracy-io-link">
+        <Button 
+          href="https://democracy.io"
           target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+          size="large" 
+          variant="contained" 
+          color="success" 
+          startIcon={<Create />}>
+          Write a Letter to Your Representatives
+        </Button>
+      </div>
     </div>
   );
 }
